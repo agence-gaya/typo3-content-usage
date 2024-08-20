3f8a9ec664c26161740a5bea6e9dea2a3da6e9b1
Benoit Chenu <bchenu@gaya.fr>
2024-08-21 00:11:06 +0200

    [BUGFIX] sys_language_uid and t3ver_wsid are always 0
    Resolves: #1

--- Classes/Domain/Repository/AbstractRepository.php
+++ Classes/Domain/Repository/AbstractRepository.php
@@ -4,19 +4,33 @@ declare(strict_types=1);
 
 namespace GAYA\ContentUsage\Domain\Repository;
 
+use GAYA\ContentUsage\Domain\Model\Content;
+use GAYA\ContentUsage\Domain\Model\Page;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\Expression\CompositeExpression;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\DataHandling\TableColumnType;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManagerInterface;
+use TYPO3\CMS\Extbase\Persistence\Generic\Mapper\ColumnMap;
 use TYPO3\CMS\Extbase\Persistence\Generic\Mapper\DataMapper;
 
 abstract class AbstractRepository
 {
     protected DataMapper $dataMapper;
 
-    public function __construct(DataMapper $dataMapper)
+    public function __construct(DataMapper $dataMapper, ObjectManagerInterface $objectManager)
     {
         $this->dataMapper = $dataMapper;
+
+        // t3ver_wsid field is not configured in TCA, so the dataMapper won't map it because it's a "non persistable property" for him
+        // To fix that, we manually add the field in the dataMapper
+        foreach ([Page::class, Content::class] as $class) {
+            $dataMap = $this->dataMapper->getDataMap($class);
+            $columnMap = $objectManager->get(ColumnMap::class, 't3ver_wsid', 't3verWsid');
+            $columnMap->setType(new TableColumnType(TableColumnType::PASSTHROUGH));
+            $dataMap->addColumnMap($columnMap);
+        }
     }
 
     abstract protected function getTableName(): string;
